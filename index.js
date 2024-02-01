67fe84a79453ba9752dcf96c1b936146287fedd1
armanphero <017911sksaifurrahman@gmail.com>
2024-02-01 15:35:53 -0800

    sdf

--- index.js
+++ index.js
@@ -22,12 +22,24 @@ app.get('/news', (req, res) => {
     res.send(news);
 })
 
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const singleNews = news.find(singleNews => singleNews._id === id)
-    res.send(singleNews)
+app.get('/trending', (req, res) => {
+    const trendingNews = news.filter(singleNews => singleNews.others_info.is_trending === true)
+    res.send(trendingNews)
 })
 
-app.listen(port, ()=> {
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const detailNews = news.find(singleNews => singleNews._id === id);
+    const similarAuthorArticles = news.filter(singleNews => singleNews.author.name === detailNews.author.name);
+
+    res.send({ detailNews, similarAuthorArticles })
+})
+
+app.get('/sports', (req, res) => {
+    const sportsNews = news.filter(singleNews => singleNews.newsCategory === "sports");
+    res.send(sportsNews);
+})
+
+app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
 })
